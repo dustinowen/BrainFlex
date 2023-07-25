b331fd67536ce67c47b3b68706d9d4b96b0b7a73
dotsiphone <dotsiphone@gmail.com>
2023-07-25 13:29:42 -0700

    Add new icons for card faces. Create event listener for turning cards over. Condense CSS using more DRY code.

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,85 @@
 /*----- constants -----*/
+const easy =[
+    {value: 1, image: './imgs/cards/easy/emoji.01.png'},
+    {value: 1, image: './imgs/cards/easy/emoji.01.png'},
+    {value: 2, image: './imgs/cards/easy/emoji.02.png'},
+    {value: 2, image: './imgs/cards/easy/emoji.02.png'},
+    {value: 3, image: './imgs/cards/easy/emoji.03.png'},
+    {value: 3, image: './imgs/cards/easy/emoji.03.png'},
+    {value: 4, image: './imgs/cards/easy/emoji.04.png'},
+    {value: 4, image: './imgs/cards/easy/emoji.04.png'},
+    {value: 5, image: './imgs/cards/easy/emoji.05.png'},
+    {value: 5, image: './imgs/cards/easy/emoji.05.png'},
+    {value: 6, image: './imgs/cards/easy/emoji.06.png'},
+    {value: 6, image: './imgs/cards/easy/emoji.06.png'},
+    {value: 7, image: './imgs/cards/easy/emoji.07.png'},
+    {value: 7, image: './imgs/cards/easy/emoji.07.png'},
+    {value: 8, image: './imgs/cards/easy/emoji.08.png'},
+    {value: 8, image: './imgs/cards/easy/emoji.08.png'},
+    {value: 9, image: './imgs/cards/easy/emoji.09.png'},
+    {value: 9, image: './imgs/cards/easy/emoji.09.png'},
+
+]
+const medium =[
+    {value: 1, image: './imgs/cards/card.01.png'},
+    {value: 1, image: './imgs/cards/card.01.png'},
+    {value: 2, image: './imgs/cards/card.02.png'},
+    {value: 2, image: './imgs/cards/card.02.png'},
+    {value: 3, image: './imgs/cards/card.03.png'},
+    {value: 3, image: './imgs/cards/card.03.png'},
+    {value: 4, image: './imgs/cards/card.04.png'},
+    {value: 4, image: './imgs/cards/card.04.png'},
+    {value: 5, image: './imgs/cards/card.05.png'},
+    {value: 5, image: './imgs/cards/card.05.png'},
+    {value: 6, image: './imgs/cards/card.06.png'},
+    {value: 6, image: './imgs/cards/card.06.png'},
+    {value: 7, image: './imgs/cards/card.07.png'},
+    {value: 7, image: './imgs/cards/card.07.png'},
+    {value: 8, image: './imgs/cards/card.08.png'},
+    {value: 8, image: './imgs/cards/card.08.png'},
+    {value: 9, image: './imgs/cards/card.09.png'},
+    {value: 9, image: './imgs/cards/card.09.png'},
+    {value: 10, image: './imgs/cards/card.10.png'},
+    {value: 10, image: './imgs/cards/card.10.png'},
+    {value: 11, image: './imgs/cards/card.11.png'},
+    {value: 11, image: './imgs/cards/card.11.png'},
+    {value: 12, image: './imgs/cards/card.12.png'},
+    {value: 12, image: './imgs/cards/card.12.png'}
+]
+const hard =[
+    {value: 1, image: './imgs/cards/card.01.png'},
+    {value: 1, image: './imgs/cards/card.01.png'},
+    {value: 2, image: './imgs/cards/card.02.png'},
+    {value: 2, image: './imgs/cards/card.02.png'},
+    {value: 3, image: './imgs/cards/card.03.png'},
+    {value: 3, image: './imgs/cards/card.03.png'},
+    {value: 4, image: './imgs/cards/card.04.png'},
+    {value: 4, image: './imgs/cards/card.04.png'},
+    {value: 5, image: './imgs/cards/card.05.png'},
+    {value: 5, image: './imgs/cards/card.05.png'},
+    {value: 6, image: './imgs/cards/card.06.png'},
+    {value: 6, image: './imgs/cards/card.06.png'},
+    {value: 7, image: './imgs/cards/card.07.png'},
+    {value: 7, image: './imgs/cards/card.07.png'},
+    {value: 8, image: './imgs/cards/card.08.png'},
+    {value: 8, image: './imgs/cards/card.08.png'},
+    {value: 9, image: './imgs/cards/card.08.png'},
+    {value: 9, image: './imgs/cards/card.08.png'},
+    {value: 10, image: './imgs/cards/card.07.png'},
+    {value: 10, image: './imgs/cards/card.07.png'},
+    {value: 11, image: './imgs/cards/card.08.png'},
+    {value: 11, image: './imgs/cards/card.08.png'},
+    {value: 12, image: './imgs/cards/card.08.png'},
+    {value: 12, image: './imgs/cards/card.08.png'},
+    {value: 13, image: './imgs/cards/card.07.png'},
+    {value: 13, image: './imgs/cards/card.07.png'},
+    {value: 14, image: './imgs/cards/card.08.png'},
+    {value: 14, image: './imgs/cards/card.08.png'},
+    {value: 15, image: './imgs/cards/card.08.png'},
+    {value: 15, image: './imgs/cards/card.08.png'}
+]
+
+
 
 
 
@@ -8,28 +89,46 @@
 /*----- state variables -----*/
 
 let levelChoice = 'easy'
-let livesEasy
-let livesMedium
-let timeHard
-
-
-
+let livesEasy = 5;
+let livesMedium = 4;
+let timeHard = 60000;
 
 
 
 
 /*----- cached elements  -----*/
 
+const squaresEls = document.querySelectorAll('.game-squares')
 
-
-
-
+shuffle(easy);
 
 
 
 
 
 /*----- event listeners -----*/
+
+squaresEls.forEach(function(el, index){
+    el.addEventListener('click', function(){
+
+        if (levelChoice == 'easy'){
+        const clickedCard = easy[index]
+        console.log(clickedCard)
+        el.setAttribute('src', clickedCard.image)
+
+    } else if (levelChoice == "medium"){
+        const clickedCard = medium[index]
+        console.log(clickedCard)
+        el.setAttribute('src', clickedCard.image)
+
+    } else {
+        const clickedCard = hard[index]
+        console.log(clickedCard)
+        el.setAttribute('src', clickedCard.image)
+        }
+    });
+});
+
 
 // TO DO - easy-medium-hard buttons=> start game 
 
@@ -47,11 +146,62 @@ let timeHard
 /*----- functions -----*/
 // init();
 
-function ren (){
-    if (levelChoice == 'easy'){
-        document.getElementsByClassName('hard').classList.add('hard-level');
-        // element.classList.add('hidden');
-    }
+function render (){
 }
 
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    
+    while (currentIndex != 0) {
+        
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+        
+        return array;
+    }
+    
+function loadBoard(){
+    if (levelChoice == 'easy'){
+        squaresEls.forEach((squareEl, index)=>{
+        let current = easy[index]
+        if (current) {
+            // squareEl.setAttribute('src', current.image)
+        }  else  {
+                squareEl.classList.add('hidden')
+            }
+        })
 
+        } else if (levelChoice == 'medium'){
+          squaresEls.forEach((squareEl, index)=>{
+            let current = medium[index]
+            if (current) {
+                // squareEl.setAttribute('src', current.image)
+            }  else  {
+                squareEl.classList.add('hidden')
+            }
+          })
+
+        } else if (levelChoice == 'hard'){
+          squaresEls.forEach((squareEl, index)=>{
+            let current = hard[index]
+            if (current) {
+                // squareEl.setAttribute('src', current.image)
+                loadTimer();
+            }  else  {
+                squareEl.classList.add('hidden')
+            }
+          })
+        }
+    }
+    
+function loadTimer(){
+    // add a timer to the board for hard level -> 60 second -> game over if not solved 
+
+}
+    
+
+loadBoard()
